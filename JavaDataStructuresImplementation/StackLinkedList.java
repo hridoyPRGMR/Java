aa97447c8cf79fa99c2e862ccb7e6260a6cc8cd6
Hridoy <118050603+hridoyPRGMR@users.noreply.github.com>
2023-12-11 19:07:12 +0600

    Update StackLinkedList.java

--- JavaDataStructuresImplementation/StackLinkedList.java
+++ JavaDataStructuresImplementation/StackLinkedList.java
@@ -46,7 +46,7 @@ class Stack {
     }
 }
 
-public class stackLinkedList {
+public class StackLinkedList{
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.push(5);
